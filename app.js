09cb5b312760c2304200235ca38e315729a33fa9
BOUAB Ahmed <bouab.ahmed757@gmail.com>
2023-04-09 03:24:37 +0100

    add sennding mail function

--- app.js
+++ app.js
@@ -11,3 +11,19 @@
     document.body.classList.toggle('light-mode');
   });
 })();
+const submitForm = (e) => {
+  e.preventDefault();
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const subject = document.getElementById('subject').value;
+  const message = document.getElementById('message').value;
+  window.location.href =
+    'mailto:bouab.ahmed757@gmail.com?subject=' +
+    encodeURIComponent(subject) +
+    '&body=' +
+    encodeURIComponent(
+      'Name: ' + name + '\nEmail: ' + email + '\nMessage: ' + message
+    );
+};
+
+document.getElementById('contactForm').addEventListener('submit', submitForm);
